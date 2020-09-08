fee346a5f90f13aba38f3b3a787ead5fb0449411
Nate Rowan <nate_rowan1@baylor.edu>
2020-09-07 23:11:05 -0500

    add greinke pitch distributions

--- scripts/repeat_pitches_01.R
+++ scripts/repeat_pitches_01.R
@@ -1,4 +1,10 @@
-library(tidyverse)
+
+
+
+
+library(tidyverse); theme_set(theme_minimal())
+library(gt)
+library(patchwork)
 
 ########## Functions ##############
 
@@ -72,6 +78,10 @@ all_data <- read_csv(
   na = c("", "NA", "null")
 )
 
+### Arrange the pitchers by name
+all_data <- all_data %>%
+  arrange_by_pitcher()
+
 ### Calculate the proportion of pitches where Zack Greinke threw his previous pitch
 all_data %>%
   filter(player_name == "Zack Greinke") %>%
@@ -91,7 +101,73 @@ all_data %>%
   add_prev_pitch() %>%
   group_by(player_name) %>%
   calc_repeat_pitch_prop() %>%
-  arrange(desc(repeat_pitch))
+  arrange(desc(repeat_pitch)) %>%
+  mutate(
+    repeat_pitch = round(repeat_pitch, 2)
+  ) %>%
+  gt()
+
+#######################################################
+### Can I predict the next pitch based on the previous pitch?
+#######################################################
+
+### Filter out only Greinke data
+greinke <- all_data %>%
+  filter(player_name == "Zack Greinke")
+
+### Add the previous pitch to each row
+greinke <- greinke %>%
+  add_prev_pitch()
+
+### Astros orange color
+astros_orange <- "#eb6e1f"
+astros_blue <- "#002d62"
+
+### Pitch proportions given he has thrown a fastball
+greinke %>%
+  filter(prev_pitch_name == "4-Seam Fastball") %>%
+  group_by(prev_pitch_name, pitch_name) %>%
+  summarise(n = n()) %>%
+  mutate(
+    prop = n / sum(n)
+  ) %>%
+  filter(n > 5) %>%
+  arrange(desc(n)) %>%
+  ggplot(aes(x = fct_reorder(pitch_name, desc(prop)), y = prop)) +
+    geom_col(fill = astros_orange) +
+    geom_text(aes(label = round(prop, 2)), vjust = -.3) +
+    labs(
+      title = "Zack Greinke Pitch Distribution After Throwing Fastball, 2019 Season",
+      subtitle = "Data via Baseball Savant",
+      x = "Pitch",
+      y = "Proportion",
+      caption = "Visualization by Nate Rowan, @nate_rowan17"
+    ) +
+    scale_y_continuous(limits = c(0, .45))
+
+### Pitch proportions in all situations
+greinke %>%
+  group_by(pitch_name) %>%
+  summarise(n = n()) %>%
+  mutate(
+    prop = n / sum(n)
+  ) %>%
+  filter(n > 10) %>%
+  arrange(desc(n)) %>%
+  ggplot(aes(x = fct_reorder(pitch_name, desc(prop)), y = prop)) +
+    geom_col(fill = astros_blue) +
+    geom_text(aes(label = round(prop, 2)), vjust = -.3) +
+    labs(
+      title = "Zack Greinke Pitch Distribution, 2019 Season",
+      subtitle = "Data via Baseball Savant",
+      x = "Pitch",
+      y = "Proportion",
+      caption = "Visualization by Nate Rowan, @nate_rowan17"
+    ) +
+    scale_y_continuous(limits = c(0, .45))
+  
+
+
 
 
 
